cae25822128c9c6bbc4864c86320ee62343e19ce
Dennis Pérez-Umaña <dennis.perez.umana@est.una.ac.cr>
2026-06-10 20:54:14 -0600

    Actualización

---
+++ app.py
@@ -0,0 +1,6 @@
+**Análisis de la situación de Geomorfositios en Costa Rica**
+
+En Costa Rica el estudio de geomorfositios data del año 2017. En ese entonces no
+se conocía del tema y prácticamente era desconocido. No obstante, con el transcurso de 
+los años las investigaciones fueron en aumento, realizándose trabajos en áreas silvestres 
+protegidas principalmente.
